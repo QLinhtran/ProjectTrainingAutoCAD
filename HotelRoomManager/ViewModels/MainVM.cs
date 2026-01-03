a2979131892e8f5b63ec81e28fc0e37a3e8b5aa5
QLinhtran <linhtrn2003@gmail.com>
2026-01-03 17:14:08 +0700

    Initial commit

---
+++ HotelRoomManager/ViewModels/MainVM.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCadAPI_demo.ViewModel
+{
+    public class MainVM
+    {
+        [CommandMethod("HOTELBOOK")]
+        public void OpenHotelUI()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+
+            ed.WriteMessage("\nHotel booking command started.");
+        }
+    }
+}
